a57b8474de65d04ea97bbec9b8a47e1f168e92ff
Matheus Coronado <matheuscoronado48@gmail.com>
2025-05-07 23:19:05 -0300

    Adicionado função para fazer a requisição e retornar o conteúdo em formato JSON

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 // Definindo a URL base da API
 const API_BASE = "https://sistemalift1.com.br/lift_ps/api/";
+
+// Função para fazer a requisição e retornar o conteúdo em formato JSON
+async function fetchJson(endpoint) {
+  const res = await fetch(`${API_BASE}${endpoint}`); // Faz a requisição para a URL da API
+  return await res.json();
+}
